0b86e3ea596bafcd4d8c8bdfd8109163095cd7f4
Evie Hoang <evie.h0325@gmail.com>
2023-10-23 22:48:50 -0700

    Added color validation to prompt for hexcodes. Added init for prompts

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const prompts = require('./lib/prompts.js')
+
+// Init prompts
+function init() {
+    inquirer
+        .prompt(prompts)
+        .then(answers => {
+            console.log(answers);
+        })
+
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+init();

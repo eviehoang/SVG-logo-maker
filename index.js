05cd6b09eeba8f5b01ec3eed9227d023cc81f2af
Evie Hoang <evie.h0325@gmail.com>
2023-10-24 20:32:39 -0700

    Deleted Keyword File, updated prompts, added to index.js to make file

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const prompts = require('./lib/prompts.js')
+const pickShape = require('./lib/shapes/pickShape.js')
+const svgImages = require('./images/image.svg')
+
+// Function to make the SVG File and add it to the folder.
+function makeSVG(response) {
+    const svg = pickShape(response);
+    fs.writeFile(svgImages, svg, () =>
+        console.log('Making Logo')
+    );
+}
 
 // Init prompts
 function init() {
     inquirer
         .prompt(prompts)
-        .then(answers => {
-            console.log(answers);
+        .then(response => {
+            console.log(response);
+            makeSVG(response);
         })
 
         .catch(err => {
